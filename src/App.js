08c7990499c78840cb5137948bd418292571888c
Kiran <smkiran.09@gmail.com>
2023-04-14 22:30:34 +0530

    Nothing updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
   return (
     <React.Fragment>
       <ForwardCounter />
+      <p>Nothing updated</p>
       <BackwardCounter />
     </React.Fragment>
   );
